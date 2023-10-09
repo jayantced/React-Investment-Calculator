33bee12325b8e566fab0066858e8991ab6e8bc4c
jayantced739 <109576199+jayantced@users.noreply.github.com>
2023-10-09 18:37:02 +0530

    created components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './assets/investment-calculator-logo.png';
-
+import Header from './Components/Header';
+import InvestmentForm from './Components/InvestmentForm';
+import InvestmentResults from './Components/InvestmentResults'
 function App() {
   const calculateHandler = (userInput) => {
     // Should be triggered when form is submitted
@@ -8,20 +9,20 @@ function App() {
     const yearlyData = []; // per-year results
 
     let currentSavings = +userInput['current-savings']; // feel free to change the shape of this input object!
-    const yearlyContribution = +userInput['yearly-contribution']; // as mentioned: feel free to change the shape...
-    const expectedReturn = +userInput['expected-return'] / 100;
-    const duration = +userInput['duration'];
+    const yearlySavings = +userInput['yearly-contribution']; // as mentioned: feel free to change the shape...
+    const expectedInterest = +userInput['expected-return'] / 100;
+    const investmentDuration = +userInput['investmentDuration'];
 
     // The below code calculates yearly results (total savings, interest etc)
-    for (let i = 0; i < duration; i++) {
-      const yearlyInterest = currentSavings * expectedReturn;
-      currentSavings += yearlyInterest + yearlyContribution;
+    for (let i = 0; i < investmentDuration; i++) {
+      const yearlyInterest = currentSavings * expectedInterest;
+      currentSavings += yearlyInterest + yearlySavings;
       yearlyData.push({
         // feel free to change the shape of the data pushed to the array!
         year: i + 1,
         yearlyInterest: yearlyInterest,
         savingsEndOfYear: currentSavings,
-        yearlyContribution: yearlyContribution,
+        yearlySavings: yearlySavings,
       });
     }
 
@@ -30,67 +31,12 @@ function App() {
 
   return (
     <div>
-      <header className="header">
-        <img src={logo} alt="logo" />
-        <h1>Investment Calculator</h1>
-      </header>
-
-      <form className="form">
-        <div className="input-group">
-          <p>
-            <label htmlFor="current-savings">Current Savings ($)</label>
-            <input type="number" id="current-savings" />
-          </p>
-          <p>
-            <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-            <input type="number" id="yearly-contribution" />
-          </p>
-        </div>
-        <div className="input-group">
-          <p>
-            <label htmlFor="expected-return">
-              Expected Interest (%, per year)
-            </label>
-            <input type="number" id="expected-return" />
-          </p>
-          <p>
-            <label htmlFor="duration">Investment Duration (years)</label>
-            <input type="number" id="duration" />
-          </p>
-        </div>
-        <p className="actions">
-          <button type="reset" className="buttonAlt">
-            Reset
-          </button>
-          <button type="submit" className="button">
-            Calculate
-          </button>
-        </p>
-      </form>
+      <Header />
 
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
-
-      <table className="result">
-        <thead>
-          <tr>
-            <th>Year</th>
-            <th>Total Savings</th>
-            <th>Interest (Year)</th>
-            <th>Total Interest</th>
-            <th>Invested Capital</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>YEAR NUMBER</td>
-            <td>TOTAL SAVINGS END OF YEAR</td>
-            <td>INTEREST GAINED IN YEAR</td>
-            <td>TOTAL INTEREST GAINED</td>
-            <td>TOTAL INVESTED CAPITAL</td>
-          </tr>
-        </tbody>
-      </table>
+    <InvestmentForm />
+      <InvestmentResults />
     </div>
   );
 }

26e9a381e1792c3d2799d439f01ff95833c17ba6
jayantced739 <109576199+jayantced@users.noreply.github.com>
2023-10-11 18:13:59 +0530

    qqqqqq

--- src/Components/InvestmentForm.js
+++ src/Components/InvestmentForm.js
@@ -3,48 +3,56 @@ import "./InvestmentForm.css";
 
 const InvestmentForm = (props) => {
 
-    const [enteredCurrentSavings, setEnteredCurrentSavings] = useState('');
-    const [enteredYearlyContribution, setEnteredYearlyContribution] = useState('');
-    const [enteredExpectedReturn, setEnteredExpectedReturn] = useState('');
-    const [enteredInvestmentDuration, setEnteredInvestmentDuration] = useState('');
+  
+  const initialUserInput = {
+        'current-savings': 10000,
+        'yearly-contribution': 1200,
+        'expected-return': 7,
+        'investmentDuration': 10
+  }
 
-    const currentSavingsHandler = (event) => {
-        setEnteredCurrentSavings(event.target.value);
-        // console.log(enteredCurrentSavings);
-    }
-    const yearlyContributionHandler = (event) => {
-        setEnteredYearlyContribution(event.target.value);
-    }
-    const expectedReturnHandler = (event) => {
-        setEnteredExpectedReturn(event.target.value);
-    }
-    const investmentDurationHandler = (event) => {
-        setEnteredInvestmentDuration(event.target.value);
-    }
-    const submitHandler = (event) => {
-        event.preventDefault();
+  const [userInput, setUserInput] = useState(initialUserInput);
 
-        const investmentData = {
-            currentSavings: enteredCurrentSavings,
-            yearlyContribution: enteredYearlyContribution,
-            expectedReturn: enteredExpectedReturn,
-            investmentDuration: enteredInvestmentDuration
-        }
-        props.onSaveInvestmentData(investmentData);
-        // console.log(investmentData);
+  const inputChangeHandler = (input, value) => {
+    console.log(input, value);
+    setUserInput((prevInput) => {
+      return {
+        ...prevInput,
+        [input] : value
+      }
+    })
+  };
 
-    }
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+  };
+  const resetHandler = () => {
+    setUserInput(initialUserInput);
+  }
 
   return (
     <form className="form" onSubmit={submitHandler}>
       <div className="input-group">
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
-          <input type="number" id="current-savings" value={enteredCurrentSavings} onChange={currentSavingsHandler} />
+          <input
+            type="number"
+            id="current-savings"
+            value={userInput['current-savings']}
+            onChange={(event) => inputChangeHandler('current-savings', event.target.value)}
+          />
         </p>
         <p>
           <label htmlFor="yearly-contribution">Yearly Contribution ($)</label>
-          <input type="number" id="yearly-contribution" value={enteredYearlyContribution} onChange={yearlyContributionHandler} />
+          <input
+            type="number"
+            id="yearly-contribution"
+            value={userInput['yearly-contribution']}
+            onChange={(event) =>
+              inputChangeHandler('yearly-contribution', event.target.value)
+            }
+          />
         </p>
       </div>
       <div className="input-group">
@@ -52,17 +60,29 @@ const InvestmentForm = (props) => {
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
           </label>
-          <input type="number" id="expected-return" value={enteredExpectedReturn} onChange={expectedReturnHandler} />
+          <input
+            type="number"
+            id="expected-return"
+            value={userInput['expected-return']}
+            onChange={(event) => inputChangeHandler('expected-return', event.target.value)}
+          />
         </p>
         <p>
           <label htmlFor="investmentDuration">
             Investment investmentDuration (years)
           </label>
-          <input type="number" id="investmentDuration" value={enteredInvestmentDuration} onChange={investmentDurationHandler} />
+          <input
+            type="number"
+            id="investmentDuration"
+            value={userInput['investmentDuration']}
+            onChange={(event) =>
+              inputChangeHandler('investmentDuration', event.target.value)
+            }
+          />
         </p>
       </div>
       <p className="actions">
-        <button type="reset" className="buttonAlt">
+        <button type="reset" className="buttonAlt" onClick={resetHandler}>
           Reset
         </button>
         <button type="submit" className="button">
--- src/Components/NewInvestment.js
+++ src/Components/NewInvestment.js
@@ -3,11 +3,14 @@ import InvestmentForm from './InvestmentForm';
 const NewInvestment = () => {
 
     const saveInvestmentDataHandler = (enteredInvestmentData) => {
-
+        const investmentData = {
+            ...enteredInvestmentData
+        }
+        console.log(investmentData);
     }
 
     return (
-        <InvestmentForm onSaveInvestmentData={saveInvestmentDataHandler}/>
+        <InvestmentForm onSaveInvestmentData={saveInvestmentDataHandler} />
     )
 }
 
